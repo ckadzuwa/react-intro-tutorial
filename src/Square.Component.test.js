13d112478d3da74d0c7242d389f3bfd1fa93eed7
Chimwemwe <ck.1@hotmail.co.uk>
2019-04-04 22:00:13 +0100

    #3 Pass value prop into square component, update tests by adding snapshots, closes #3

--- src/Square.Component.test.js
+++ src/Square.Component.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Square from './Square.Component'
 
 configure({ adapter: new Adapter() });
@@ -9,4 +9,19 @@ configure({ adapter: new Adapter() });
 test('it should render without crashing', () => {
     const square = shallow(<Square/>);
     expect(square).toHaveLength(1);
+});
+
+test('it should render without crashing when props are passed in', () => {
+    const square = shallow(<Square value={1}/>);
+    expect(square).toHaveLength(1);
+});
+
+test('it should should match the snapshot', () => {
+    const square = shallow(<Square value={1}/>);
+    expect(square).toMatchSnapshot();
+});
+
+test('it should render a button with text 1 inside', () => {
+    const square = shallow(<Square value={1}/>);
+    expect(square.html()).toEqual("<button class=\"square\">1</button>");
 });
