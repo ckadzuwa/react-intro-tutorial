fe49070c6eeae74a5e67658f58809b212c81a955
Chimwemwe <ck.1@hotmail.co.uk>
2019-04-06 21:53:44 +0100

    #11 Lift state of square value from Square to Board component, update tests

--- src/Board.Component.test.js
+++ src/Board.Component.test.js
@@ -22,3 +22,23 @@ test('it should 3x3 grid giving a total of nine squares', () => {
     expect(board.find('.board-row')).toHaveLength(3);
     expect(board.find(Square)).toHaveLength(9);
 });
+
+test('it should update internal state when a square is clicked', () => {
+    const board = mount(<Board/>);
+    const firstSquare = board.find(Square).first();
+    expect(board.state('squares')).toEqual(Array(9).fill(null));
+   
+    firstSquare.find('button').simulate('click');
+
+    expect(board.state('squares')).toEqual(['X', ...Array(8).fill(null)]);
+});
+
+test('it should update the sqaure clicked with an X', () => {
+    const board = mount(<Board/>);
+    const firstSquare = board.find(Square).first();
+    expect(firstSquare.html()).toEqual('<button class=\"square\"></button>');
+
+    firstSquare.find('button').simulate('click');
+
+    expect(firstSquare.html()).toEqual('<button class=\"square\">X</button>');
+});
--- src/Square.Component.js
+++ src/Square.Component.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        value: null,
-      };
-    }
-
-    render() {
-      return (
-        <button className="square" onClick={() => this.setState({ value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
+}
